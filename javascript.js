951e15c84ccc3df7bfc908574187667fa9621486
Vicker <vcalero2001@gmail.com>
2025-06-25 18:14:05 +0200

    Implemented game logic for console gameplay

---
+++ javascript.js
@@ -0,0 +1,89 @@
+const GameBoard = (() => {
+
+    const board = [
+        "-", "-", "-",
+        "-", "-", "-",
+        "-", "-", "-"
+    ]
+    const getBoard = () => board;
+    const printBoard = () => {console.log(
+        `
+        ${board[0]} || ${board[1]} || ${board[2]} \n
+        ${board[3]} || ${board[4]} || ${board[5]} \n
+        ${board[6]} || ${board[7]} || ${board[8]}
+        `
+    )}
+
+    const setCell = (index, mark) => {
+        if (board[index] === "-") {
+            board[index] = mark;
+        }
+    }
+
+    const reset = () => {
+        for (let index = 0; index < board.length; index++) {
+            board[index] = "-";
+        }
+    }
+
+    return {getBoard, setCell, reset, printBoard};
+
+})();
+
+const Player = (pName, mark) => { 
+    const playerWins = 0;
+    return {pName, mark, playerWins}
+};
+
+const GameController = (() => {
+
+    const player1 = Player("Player 1", "X");
+    const player2 = Player("Player 2", "O");
+
+    let currentPlayer = player1;
+
+    const switchTurn = () => {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    }
+
+    const playRound = (index) => {
+
+        if (GameBoard.getBoard()[index] === "-") {
+
+            GameBoard.setCell(index, currentPlayer.mark);
+            GameBoard.printBoard();
+
+            if (hasWinner()) {
+                console.log(`The winner is ${currentPlayer.pName}`)
+                GameController.resetGame();
+            } else switchTurn();
+        }
+        else console.log("Jugada invalida");
+    }
+
+    const resetGame = () => {
+        GameBoard.reset();
+        currentPlayer = player1;
+    }
+
+    const hasWinner = () => {
+        const board = GameBoard.getBoard();
+        if (
+            board[0] === board[1] && board[0] === board[2] && board[0] !== "-" ||
+            board[3] === board[4] && board[3] === board[5] && board[3] !== "-" ||
+            board[6] === board[7] && board[6] === board[8] && board[6] !== "-" ||
+        // Check lines
+            board[0] === board[3] && board[0] === board[6] && board[0] !== "-" ||
+            board[1] === board[4] && board[1] === board[7] && board[1] !== "-" ||
+            board[2] === board[5] && board[2] === board[8] && board[2] !== "-" ||
+        // Check columns
+            board[0] === board[4] && board[0] === board[8] && board[0] !== "-" ||
+            board[2] === board[4] && board[2] === board[6] && board[2] !== "-"
+        // Check diagonal
+        ) return true;
+
+        return false;
+    }
+
+    return {playRound, resetGame}
+}) ();
